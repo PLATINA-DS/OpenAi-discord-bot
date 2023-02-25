62c67166ef758026bce9cb9220d4c29e8dab3ef7
PLATINA <73738047+PLATINA-DS@users.noreply.github.com>
2023-02-25 23:02:46 +0100

    Update cooldown.py

--- cooldown_factory/cooldown.py
+++ cooldown_factory/cooldown.py
@@ -3,5 +3,5 @@ from discord import app_commands
 from typing import Optional
 
 
-def ask_command_cooldown(interaction: discord.Interaction) -> Optional[app_commands.Cooldown]:
+def ask_command_cooldown(interaction: discord.Interaction) -> app_commands.Cooldown:
     return app_commands.Cooldown(1, 20.0)
