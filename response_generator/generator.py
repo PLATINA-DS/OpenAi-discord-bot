ccaa4a3557cfa3a237eab4dac41cf6294aa48833
PLATINA <73738047+PLATINA-DS@users.noreply.github.com>
2023-02-26 01:16:34 +0100

    Update generator.py
    The code was refactored, sunc requests were changed to async ones

--- response_generator/generator.py
+++ response_generator/generator.py
@@ -1,28 +1,47 @@
 import discord
 from time import time
-import openai
-from typing import Optional
+import aiohttp
 
-from settings import embed_color
+from settings import embed_color, OPENAI_API_KEY
 
 
-async def generator(model: str, deco_model_name: str, prompt: str) -> Optional[discord.Embed]:
-    generation_start = time()
-    response = openai.Completion.create(
-        model=model,
-        prompt=prompt,
-        max_tokens=1024,
-        temperature=0.4
-    )
-    generation_finish = time()
-    generation_time = int(generation_finish - generation_start)
-    answer = response.get('choices')[0].get('text')
-    answer = f"{answer[:1021]}..." if len(answer) > 1024 else answer
+async def generate(model: str, prompt: str) -> str:
+    async with aiohttp.ClientSession() as session:
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {OPENAI_API_KEY}'
+        }
+        data = {
+            'prompt': prompt,
+            'temperature': 0.7,
+            'max_tokens': 1024
+        }
+
+        async with session.post(f'https://api.openai.com/v1/engines/{model}/completions',
+                                headers=headers, json=data) as resp:
+            response_data = await resp.json()
+            response_text = response_data.get("choices")[0].get("text")
+
+    answer = f"{response_text[:1021]}..." if len(response_text) > 1024 else response_text
+
+    return answer
+
+
+async def timer(model: str, prompt: str) -> int and str:
+    start = time()
+    answer = await generate(model, prompt)
+    end = time()
+    result = int(end - start)
+
+    return result, answer
+
+
+async def generated_embed(model: str, deco_model_name: str, prompt: str) -> discord.Embed:
+    generation_time, answer = await timer(model, prompt)
     embed = discord.Embed(
         title="Result",
         color=embed_color
     )
-
     embed.set_author(name=f"Model - {deco_model_name}")
     embed.add_field(name="Question", value=prompt, inline=False)
     embed.add_field(name="Answer", value=answer, inline=False)
